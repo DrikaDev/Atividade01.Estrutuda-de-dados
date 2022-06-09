a4a2b44ff7c09b6f2311324ab2009455bbfc3222
DrikaDev <adriguti.adriana@hotmail.com>
2022-06-09 14:13:35 -0300

    Atividade01.Estrutuda de dados

---
+++ script.js
@@ -0,0 +1,14 @@
+const nomeCompleto = "Adriana Gutierrez"
+let idade = 42
+let anoAtual = 2022
+let resultado = anoAtual - idade
+
+console.log(resultado);
+
+console.log(`Estou mexendo no console.log!`);
+
+let quartaFeira = false
+console.log(typeof quartaFeira);
+
+
+
