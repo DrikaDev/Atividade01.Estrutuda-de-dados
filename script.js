765fc45affb29a11488df0cb03ef49435100383f
Adriana Gutierrez <102387476+DrikaDev@users.noreply.github.com>
2022-06-13 07:37:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+// Crie uma variável que receba um nome;
 const nomeCompleto = "Adriana Gutierrez"
+
+// Crie uma variável que receba um número;
 let idade = 42
-let anoAtual = 2022
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+let anoAtual = 2022 // ou é assim? let anoAtual = 2022 - idade
 let resultado = anoAtual - idade
 
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
 console.log(resultado);
 
+//Mostre no console a mensagem "Estou mexendo no console.log";
 console.log(`Estou mexendo no console.log!`);
 
+/* Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa 
+verdadeiro ou falso e mostre a saída no console;*/
 let quartaFeira = false
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
 console.log(typeof quartaFeira);
 
 
